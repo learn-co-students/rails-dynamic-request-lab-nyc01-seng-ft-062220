dac95994e37e104f7242b1cdd8d6487cf6d24ce1
Michael Horowitz <mjhorowitz714@yahoo.com>
2020-07-17 13:31:03 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :students, only: :index
+
+  # This show route can be refactored into the above resouces' method call above, like so:
+  # resources :students, only: [:index, :show]
+  get "students/:id", to: "students#show"
 end
